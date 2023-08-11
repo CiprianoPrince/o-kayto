bfffdda07a10edebd43e425c5fd8dec86e7fc9b0
Maricris De Jesus <princetonjudecipriano.com>
2023-08-11 23:43:33 +0800

    added generateFakeUser helper fn + add faker library + updated down fn of seeders

--- server/seeders/20230811143839-user.seeder.js
+++ server/seeders/20230811143839-user.seeder.js
@@ -1,25 +1,23 @@
-'use strict';
+"use strict"
+
+const generateFakeUser = require("../helpers/generateFakeUser")
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
+  async up(queryInterface, Sequelize) {
     /**
      * Add seed commands here.
      *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+     Example:
+     */
+    await queryInterface.bulkInsert(
+      "Users",
+      new Array(100).fill(undefined).map(generateFakeUser),
+      {}
+    )
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-  }
-};
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete("Users", null, {})
+  },
+}
