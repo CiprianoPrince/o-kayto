7071572e21b9362187dded79655ae46ef06b9daf
Glenn Evangelista <glennmarquez28@gmail.com>
2023-08-16 15:18:30 +0800

    create signin form

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,19 +1,23 @@
 import { Container } from "react-bootstrap"
 import { Main } from "../../components/layout/Frontend"
 import { Link } from "react-router-dom"
+import Signin from "../Signin/Signin"
+
 
 const heroContainerCss = ""
 
 const Home = () => {
   return (
     <Main>
-      <Container className='bg-success h-75' fluid as='section'>
+
+    <Signin/>
+      {/* <Container className='bg-success h-75' fluid as='section'>
         <h1>Dress the Best, Forget the Rest:</h1>
         <p>Discover the latest trends and timeless classics.</p>
         <Link to='collection'>Check Collection</Link>
       </Container>
-
-      <Container fluid as='section'></Container>
+ 
+      <Container fluid as='section'></Container> */}
     </Main>
   )
 }
--- client/src/pages/Signin/Signin.jsx
+++ client/src/pages/Signin/Signin.jsx
@@ -2,30 +2,35 @@ import React from 'react'
 
 function Signin() {
   return (
-    <div>
-      <form>
+   
 
-  <div class="form-outline mb-4">
-    <input type="email" id="form2Example1" class="form-control" />
-    <label class="form-label" for="form2Example1">Email address</label>
-  </div>
+  <div className='container'>
+   
+    <div className='bg-white p-3 rounded w-25 border m-3' >
+      <div className="text-center">
+        <h3>O-kayto Login</h3>
+        <p>New here? <a href="#!">Create an account</a></p>
+      </div>
+    <form>
+    <div className="form-outline mb-4">
+       <label className="form-label" for="form2Example1">Email address</label>
+       <input type="email" id="form2Example1" className="form-control" placeholder='juandelacruz@email.com' />
+    </div>
 
+    <div className="form-outline mb-4">
+       <label className="form-label" for="form2Example2">Password</label>
+       <input type="password" id="form2Example2" className="form-control" placeholder='Password' />
+    </div>
 
-  <div class="form-outline mb-4">
-    <input type="password" id="form2Example2" class="form-control" />
-    <label class="form-label" for="form2Example2">Password</label>
-  </div>
-
-  <div class="row mb-4">
-    <div class="col d-flex justify-content-center">
-
-      <div class="form-check">
-        <input class="form-check-input" type="checkbox" value="" id="form2Example34" checked />
-        <label class="form-check-label" for="form2Example34"> Remember me </label>
+    <div className="row mb-4">
+      <div className="col d-flex justify-content-center">
+        <div className="form-check">
+          <input className="form-check-input" type="checkbox" value="" id="form2Example34" checked />
+          <label className="form-check-label" for="form2Example34"> Remember me </label>
       </div>
     </div>
 
-    <div class="col">
+    <div className="col">
   
       <a href="#!">Forgot password?</a>
     </div>
@@ -34,26 +39,27 @@ function Signin() {
   <button type="submit" class="btn btn-primary btn-block mb-4">Sign in</button>
 
   
-  <div class="text-center">
-    <p>Not a member? <a href="#!">Register</a></p>
+  <div className="text-center">
     <p>or sign up with:</p>
-    <button type="button" class="btn btn-secondary btn-floating mx-1">
-      <i class="fab fa-facebook-f"></i>
+    <button type="button" className="btn btn-secondary btn-floating mx-1">
+      <i className="fab fa-facebook-f"></i>
     </button>
 
-    <button type="button" class="btn btn-secondary btn-floating mx-1">
-      <i class="fab fa-google"></i>
+    <button type="button" className="btn btn-secondary btn-floating mx-1">
+      <i className="fab fa-google"></i>
     </button>
 
-    <button type="button" class="btn btn-secondary btn-floating mx-1">
-      <i class="fab fa-twitter"></i>
+    <button type="button" className="btn btn-secondary btn-floating mx-1">
+      <i className="fab fa-twitter"></i>
     </button>
 
-    <button type="button" class="btn btn-secondary btn-floating mx-1">
-      <i class="fab fa-github"></i>
+    <button type="button" className="btn btn-secondary btn-floating mx-1">
+      <i className="fab fa-github"></i>
     </button>
   </div>
 </form>
+<br />
+    </div>
     </div>
   )
 }
