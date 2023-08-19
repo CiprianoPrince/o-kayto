49472317c69852d12d9053a82e7ea35dce54e646
Charlie Sacro <charlacro.e@gmail.com>
2023-08-19 21:04:20 +0800

    Done with the cart and wishlist

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,19 +1,23 @@
 import { Container } from "react-bootstrap"
 import { Main } from "../../components/layout/Frontend"
 import { Link } from "react-router-dom"
+import Cart from "../Cart/Cart"
+import Wishlist from "../Wishlist/Wishlist"
 
 const heroContainerCss = ""
 
 const Home = () => {
   return (
     <Main>
-      <Container className='bg-success h-75' fluid as='section'>
+      <Wishlist/>
+      {/* <Cart/> */}
+      {/* <Container className='bg-success h-75' fluid as='section'>
         <h1>Dress the Best, Forget the Rest:</h1>
         <p>Discover the latest trends and timeless classics.</p>
         <Link to='collection'>Check Collection</Link>
       </Container>
 
-      <Container fluid as='section'></Container>
+      <Container fluid as='section'></Container> */}
     </Main>
   )
 }
